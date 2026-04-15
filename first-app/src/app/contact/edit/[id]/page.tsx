eeb7396c37f765953905ced5d5e0f65c78038777
Shubham Kumar <shubham620409@gmail.com>
2026-04-15 23:00:29 +0530

    First application

---
+++ first-app/src/app/contact/edit/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FirstPage = () => {
+  return (
+    <div>
+      This is first edit page
+    </div>
+  )
+}
+
+export default FirstPage
---
+++ first-app/src/app/contact/new/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewPage = () => {
+  return (
+    <div>
+      This is new contact Page
+    </div>
+  )
+}
+
+export default NewPage
